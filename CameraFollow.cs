a55a146801de968785b8fc4053d1f48776070448
Dowen <terryden5566@yahoo.com.tw>
2014-12-13 00:55:39 +0800

    ScriptInitialize

---
+++ CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public PlayerController target;//the target object
+	//private float horizontalSpeed = 40.0f;
+	//private float verticalSpeed = 40.0f;
+	private Vector3 targetOldPosition;
+	private float speedMod = 1.0f;//a speed modifier
+	private Vector3 point;//the coord to the point where the camera looks at
+
+
+	void Start () {//Set up things on the start method
+		//point = target.transform.position;//get target's coords
+		transform.LookAt(target.transform.position);//makes the camera look to it 
+		target.SetForwardDirection(Camera.main.transform.up);
+		target.SetRightDirection (Camera.main.transform.right);
+	}
+	
+	
+	
+	void Update () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
+		float delta_x =  target.transform.position.x - targetOldPosition.x;
+		float delta_y = target.transform.position.y - targetOldPosition.y;
+		float delta_z = target.transform.position.z - targetOldPosition.z;
+		float locate_x = this.transform.position.x + delta_x;
+		float locate_y = this.transform.position.y + delta_y;
+		float locate_z = this.transform.position.z + delta_z;
+		this.transform.position = new Vector3 (locate_x, locate_y, locate_z);
+		if(Input.GetMouseButton (1))
+		{
+			//float h = horizontalSpeed * Input.GetAxis ("Mouse Y");
+			float v = Input.GetAxis ("Mouse X");
+			transform.RotateAround (target.transform.position,new Vector3(0.0f,1.0f,0.0f),1 * v * speedMod);
+			//transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+		}
+		target.SetForwardDirection(Camera.main.transform.up);
+		target.SetRightDirection (Camera.main.transform.right);
+		targetOldPosition = target.transform.position;
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	private Vector3 forwardDirection,rightDirection;
+	public float ForwardSpeed = 0.1f;
+	public float BackwardSpeed = 0.1f;
+	public float SideSpeed = 0.1f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float pX = this.transform.position.x;
+		float pZ = this.transform.position.z;
+		float pY = this.transform.position.y;
+
+		if (Input.GetKey (KeyCode.W))
+		{
+			pX += forwardDirection.x * ForwardSpeed;
+			pY += forwardDirection.y * ForwardSpeed;
+			pZ += forwardDirection.z * ForwardSpeed;
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			pX -= forwardDirection.x * BackwardSpeed;
+			pY -= forwardDirection.y * BackwardSpeed;
+			pZ -= forwardDirection.z * BackwardSpeed;
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			pX -= rightDirection.x * SideSpeed;
+			pY -= rightDirection.y * SideSpeed;
+			pZ -= rightDirection.z * SideSpeed;
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			pX += rightDirection.x * SideSpeed;
+			pY += rightDirection.y * SideSpeed;
+			pZ += rightDirection.z * SideSpeed;
+		}
+		this.transform.position = new Vector3 (pX, pY, pZ);
+
+	}
+
+	public void SetForwardDirection(Vector3 forward)
+	{
+		forwardDirection = forward;
+		forwardDirection.y = 0;
+	}
+
+	public void SetRightDirection(Vector3 right)
+	{
+		rightDirection = right;
+		rightDirection.y = 0;
+	}
+
+}
