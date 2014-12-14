377c436a8e8aeb25e802656cd1dbcea234a1adc2
Dowen <terryden5566@yahoo.com.tw>
2014-12-14 23:15:43 +0800

    Communicate with Npc

---
+++ Communicate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Communicate : MonoBehaviour {
+
+	public Text Name;
+	public Text Content;
+	public Image TitleBackground;
+	public Image ContentBackground;
+
+	// Use this for initialization
+	void Start () {
+		OpenDialog (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OpenDialog(bool open)
+	{
+		bool active = open;
+		Name.gameObject.SetActive(active);
+		Content.gameObject.SetActive(active);
+		TitleBackground.gameObject.SetActive(active);
+		ContentBackground.gameObject.SetActive(active);
+	}
+
+	public void SetContent(string content)
+	{
+		Content.text = content;
+	}
+
+	public void SetName(string name)
+	{
+		Name.text = name;
+	}	
+
+	public void SetTitleBackground(Color color)
+	{
+		TitleBackground.color = color;
+	}
+	public void SetContentBackground(Color color)
+	{
+		ContentBackground.color = color;
+	}
+}
---
+++ Communicator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Communicator : MonoBehaviour {
+
+	public Communicate Communicate;
+	public string Name;
+	public string[] Contents;
+	public Color TitleBackground;
+	public Color ContentBackground;
+	private bool openDialog = false;
+	private bool inRange = false;
+	private int dialogIndex = -1;
+
+	// Use this for initialization
+	void Start () {
+		openDialog = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(openDialog && Input.GetKeyDown(KeyCode.Space))
+		{
+			Talking();
+		}
+		else if(Input.GetKeyDown(KeyCode.E) && inRange && !openDialog)
+		{
+			openDialog = true;
+			Communicate.OpenDialog(openDialog);
+			Talking();
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "PLAYER" && !inRange)
+		{
+			inRange = true;
+			Communicate.OpenDialog(false);
+			Communicate.SetName(Name);;
+			Communicate.SetTitleBackground(TitleBackground);
+			Communicate.SetContentBackground(ContentBackground);
+		}
+		else
+		{
+			inRange = false;
+		}
+	}
+
+	bool Talking()
+	{
+		dialogIndex++;
+		if (dialogIndex >= Contents.Length)
+		{
+			Reset();
+			return false;
+		}
+		Communicate.SetContent (Contents[dialogIndex]);
+		return true;
+	}
+
+	private void Reset()
+	{
+		inRange = false;
+		openDialog = false;
+		dialogIndex = -1;
+		Communicate.OpenDialog(false);
+	}
+}
